e0ca71d28b3462747552cdee4b0de9bac1fe732a
Evgeniy <libertydreamstudia@gmail.com>
2018-11-29 01:13:29 +0300

    UP

---
+++ user/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CyberBattle</title>
+	<link rel="stylesheet" type="text/css" href="resource/css/main.css">
+</head>
+<body>
+	<div class="top-menu">
+		<div class="top-menu-left">
+			<a class="home"> </a>
+			<a class="button1">sdfdsf </a>
+		</div>
+		<div class="top-menu-right">
+			
+		</div>
+	</div>
+	<div class="left"> 
+		<div class="left-list">
+			
+		</div>
+		<div class="left-nav-menu">
+			
+		</div>
+		<div class="content"></div>
+	</div>
+			
+
+</body>
+</html>
+<?
+?>
