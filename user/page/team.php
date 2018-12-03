4c11f67d8393cc7f0770ba43114566378919963f
Evgeniy <libertydreamstudia@gmail.com>
2018-12-03 09:29:58 +0300

    UP

---
+++ user/page/team.php
@@ -0,0 +1,46 @@
+<div class="play-block">
+	<div class="left-nav-menu__title">
+		<h6>Команды</h6>
+		<hr>
+	</div>
+	<button class="but2">Список всех команд</button>
+	<button class="but2">Создать команду</button>
+	
+	<div class="block-panel">
+		<h6 class="title-panel">Ваши команды<hr></h6>	
+		<div class="team-item">
+			<img src="resource/img/clashroyale.png">
+			<span class="game-title">ClashRoyale 5000$</span>
+			<i class="fas fa-times-circle icon" ></i>		
+		</div>
+		<div class="team-item">
+			<img src="resource/img/pubgmobile.jpg">
+			<span class="game-title">PubgMobile Zone</span>	
+			<i class="fas fa-times-circle icon" ></i>	
+		</div>
+		<div class="team-item">
+			<img src="resource/img/hs.png">
+			<span class="game-title">Hearstone Zone</span>	
+			<i class="fas fa-times-circle icon" ></i>	
+		</div>
+	</div>
+	<div class="block-panel">
+		<h6 class="title-panel">Топ-10<hr></h6>	
+		<div class="team-item">
+			<img src="resource/img/clashroyale.png">
+			<span class="game-title">ClashRoyale 5000$</span>
+			<i class="fas fa-times-circle icon" ></i>		
+		</div>
+		<div class="team-item">
+			<img src="resource/img/pubgmobile.jpg">
+			<span class="game-title">PubgMobile Zone</span>	
+			<i class="fas fa-times-circle icon" ></i>	
+		</div>
+		<div class="team-item">
+			<img src="resource/img/hs.png">
+			<span class="game-title">Hearstone Zone</span>	
+			<i class="fas fa-times-circle icon" ></i>	
+		</div>
+	</div>
+</div>
+
